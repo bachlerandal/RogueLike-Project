d9ce7ae9804922144933966a7d85946f188bf411
NobleOrder <jralston@unomaha.edu>
2019-02-03 22:24:30 -0600

    Milestone 2
    - Added basic spell "fireball"
    - Edited enemy prefab
    - Created 3 Scripts: EnemyStats, PlayerMagic and Projectile

---
+++ WorkInProgress/Test Room 2D/Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+
+    public int health = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void takeDamage(int damage)
+    {
+        health -= damage;
+    }
+}
---
+++ WorkInProgress/Test Room 2D/Assets/Scripts/PlayerMagic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMagic : MonoBehaviour
+{
+
+    public GameObject fireball;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            shoot();
+        }
+    }
+
+    private void shoot()
+    {
+        Instantiate(fireball, transform.position, transform.rotation);
+    }
+}
