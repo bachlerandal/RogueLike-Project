b306c26fd0afc4ccdfe8b6b05c5aad7dafdd62f5
NobleOrder <jralston@unomaha.edu>
2019-02-11 23:13:16 -0600

    idk
    idk

---
+++ WorkInProgress/Test Room 2D/Assets/Scripts/Projectile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public float moveSpeed = 3f;
+    public int damage = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        transform.position = new Vector2(
+            transform.position.x + (moveSpeed * Time.deltaTime),
+            transform.position.y
+            );
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        EnemyStats enemy = collision.gameObject.GetComponent<EnemyStats>();
+
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            enemy.takeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
